f5f54582323f97d36ca75516ea4acbf66d52cb7c
DannyM1chael <coldsoulrusirk@gmail.com>
2020-06-28 19:14:18 +0300

    html coding, add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import Header from './components/Header';
+import Category from './components/Category';
+import PizzaBlock from './components/screens/pizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Category />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartBtn from '../CartBtn';
+
+export default function Header() {
+  const logoImg = process.env.PUBLIC_URL + './assets/img/pizza-logo.svg';
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="header__logo">
+          <img width="38" src={logoImg} alt="Pizza logo" />
+          <div>
+            <h1>Pizza Delivery</h1>
+            <p>Order our best pizza today!</p>
+          </div>
+        </div>
+        <CartBtn />
+      </div>
+    </div>
+  );
+}
