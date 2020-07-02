4b3b9ed5ab666abdd0d712fb2979beccae7b7551
DannyM1chael <coldsoulrusirk@gmail.com>
2020-07-02 16:37:46 +0300

    add scripts, add concurrently

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,17 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Spinner from './components/Spinner';
-import configureStore, { history } from './store/conf';
 import './index.sass';
-
-// const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <Router history={history}>
+    <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <App />
       </Suspense>
-    </Router>
-    {/* </Provider> */}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
